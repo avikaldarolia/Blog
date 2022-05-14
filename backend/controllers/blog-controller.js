fbfbba3846d36a774e917b3d223c05fc36d2f771
Avikal Darolia <avikaldarolia7@gmail.com>
2022-05-14 17:53:17 +0530

    Update a blog

--- backend/controllers/blog-controller.js
+++ backend/controllers/blog-controller.js
@@ -12,3 +12,37 @@ export const getAllBlogs = async (req, res, next) => {
   }
   return res.status(200).json({ blogs });
 };
+
+export const addBlog = async (req, res, next) => {
+  const { title, image, description, user } = req.body;
+  const blog = new Blog({
+    title,
+    description,
+    image,
+    user,
+  });
+  try {
+    await blog.save();
+  } catch (error) {
+    return console.log(error);
+  }
+  return res.status(200).json({ blog });
+};
+
+export const updateBlog = async (req, res, next) => {
+  const { title, description } = req.body;
+  const blogId = req.params.id;
+  let blog;
+  try {
+    blog = await Blog.findByIdAndUpdate(blogId, {
+      title,
+      description,
+    });
+  } catch (error) {
+    return console.log(error);
+  }
+  if (!blog) {
+    return res.status(500).json({ message: 'Unable to update the blog' });
+  }
+  return res.status(200).json({ blog });
+};
--- backend/routes/blog-routes.js
+++ backend/routes/blog-routes.js
@@ -1,8 +1,14 @@
 import express from 'express';
-import { getAllBlogs } from '../controllers/blog-controller';
+import {
+  addBlog,
+  getAllBlogs,
+  updateBlog,
+} from '../controllers/blog-controller';
 
 const blogRouter = express.Router();
 
 blogRouter.get('/', getAllBlogs);
+blogRouter.post('/add', addBlog);
+blogRouter.put('/update/:id', updateBlog);
 
 export default blogRouter;
