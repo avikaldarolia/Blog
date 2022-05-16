db2f4299c33374cd7fde7db36c65854c5ea7234e
Avikal Darolia <avikaldarolia7@gmail.com>
2022-05-16 19:22:51 +0530

    Finished fetching blogs(including blogs of the logged in user)

--- frontend/src/components/UserBlogs.jsx
+++ frontend/src/components/UserBlogs.jsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Blog from './Blog';
 
 const UserBlogs = () => {
-  return <div>UserBlogs</div>;
+  const id = localStorage.getItem('userId');
+  const [blogs, setBlogs] = useState();
+  const sendRequest = async () => {
+    const res = await axios
+      .get(`http://localhost:5000/api/blog/user/${id}`)
+      .catch((err) => console.log(err));
+    const data = await res.data;
+    return data;
+  };
+  useEffect(() => {
+    sendRequest().then((data) => setBlogs(data.blogs.blogs));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  console.log(blogs);
+  return (
+    <div>
+      {' '}
+      {blogs &&
+        blogs.map((blog, index) => (
+          <Blog
+            title={blog.title}
+            description={blog.description}
+            imageUrl={blog.image}
+            userName={blog.user}
+          />
+        ))}
+    </div>
+  );
 };
 
 export default UserBlogs;
